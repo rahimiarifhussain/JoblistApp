439cffb10d10472e0e68f1603960ee61655c8b73
Arif Hussain Rahimi <arifhussain.rahimi84@gmail.com>
2025-01-18 14:14:26 +0430

    use react route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Hero from "./components/Hero"
 import HomeCard from "./components/HomeCard";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar"
 import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route path="/home" element={<HomePage />} />
+    </Route>
+  ));
 function App() {
-  const title = "Become a React Dev" ;
-  const subtitle = "Find the React Job that fits your skills and needs";
-  return (
-   <>
-   
-    <Navbar />
-    <Hero title={title} subtitle={subtitle}  />
-
-    <HomeCard />
-
-    <JobListings />
-  
-    <ViewAllJobs />
-   
-   </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
+
+
+const HomePage = () => {
+  return (
+   <>
+          
+          <Hero />
+   </>
+  )
+}
+
+export default HomePage
